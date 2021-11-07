f608e53b1ab0ce144c7d40bba50a8fb3a59187e3
TianRui <liutia20@msu.edu>
2021-11-07 10:58:06 -0600

    UPDATE

--- leetcode/editor/cn/[1]Two Sum.py
+++ leetcode/editor/cn/[1]Two Sum.py
@@ -47,5 +47,8 @@
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        pass
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
 # leetcode submit region end(Prohibit modification and deletion)
